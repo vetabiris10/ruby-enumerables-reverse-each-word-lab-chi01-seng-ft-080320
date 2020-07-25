60d2b5ed0ea48b22336f232876641af24d9c434a
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-25 01:45:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string1)
+  string1.each do |element|
+    element.reverse
+  end
+
+end
+
+def reverse_each_word(string)
+  string2.collect do |element|
+    element.reverse
+  end
+
+end
