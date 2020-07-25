cabd41720db5f2cc6f3499856ad56a05eff8b9c1
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-25 03:52:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,12 @@ def reverse_each_word(string1)
 end
 
 def reverse_each_word(string)
-
+  new_string = []
   string.split(' ').collect do |element|
     element.reverse!
-    string.join
+    new_string.join
   end
 end
 
 
-  new_string = []
+
